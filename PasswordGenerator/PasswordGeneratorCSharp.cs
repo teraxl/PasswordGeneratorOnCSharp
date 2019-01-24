7e80bd5a7ebdf16a3d7fcef1425a5f42acbaeb1e
teraxl <muga.alexandr@yandex.ru>
2019-01-24 19:21:41 +0300

    add class

---
+++ PasswordGenerator/PasswordGeneratorCSharp.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: teraxl
+ * Date: 24.01.2019
+ * Time: 19:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace PasswordGenerator
+{
+	/// <summary>
+	/// Description of PasswordGeneratorCSharp.
+	/// </summary>
+	public class PasswordGeneratorCSharp
+	{
+		public PasswordGeneratorCSharp()
+		{
+		}
+	}
+}
